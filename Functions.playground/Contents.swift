31062db38623de86e173fa6cc55680012cd66859
sudeepa88 <palsudeepa@gmail.com>
2024-04-14 09:42:43 +0530

    Closures Added

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,2 +1,21 @@
 
-var greeting = "Hello, playground"
+//var greeting = "Hello, playground"
+
+func greeting(_ person: String) -> String {
+    "Hello, " + person + "!"
+}
+print(greeting("Me"))
+
+
+func arithmeticMean(_ numbers: Double...) -> Double {
+    var total: Double = 0
+    for number in numbers {
+        total += number
+    }
+    var root = total / Double(numbers.count)
+    print(root)
+    return root
+}
+
+
+arithmeticMean(1, 2, 3, 4, 5)
